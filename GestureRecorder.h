965ae76854d05abc3ad25cbe974e8711350e3a8f
ziadsaeed2434-web <ziadsaeed2434@gmail.com>
2026-09-14 20:10:41 +0300

    Update GestureRecorder.h

--- GestureRecorder.h
+++ GestureRecorder.h
@@ -1,9 +1,14 @@
-// GestureRecorder.h
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 @interface GestureRecorder : NSObject
+
 + (instancetype)sharedInstance;
 - (void)show;
 - (void)hide;
+- (void)flashIndicatorAtPoint:(CGPoint)point;
+
+@property (nonatomic, assign) BOOL isRecording;
+@property (nonatomic, strong) NSMutableArray *recordedTouches;
+
 @end
