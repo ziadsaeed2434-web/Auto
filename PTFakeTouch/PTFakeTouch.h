e2439d598e0f902583103de5b802fbaed247af78
ziadsaeed2434-web <ziadsaeed2434@gmail.com>
2026-09-14 20:09:49 +0300

    Update PTFakeTouch.h

--- PTFakeTouch/PTFakeTouch.h
+++ PTFakeTouch/PTFakeTouch.h
@@ -3,7 +3,9 @@
 #import <UIKit/UIKit.h>
 
 @interface PTFakeTouch : NSObject
-+ (NSInteger)fakeTouchId:(NSInteger)pointId atPoint:(CGPoint)point withPhase:(UITouchPhase)phase;
++ (NSInteger)fakeTouchId:(NSInteger)pointId
+                 AtPoint:(CGPoint)point
+          withTouchPhase:(UITouchPhase)phase;
 + (NSInteger)getAvailablePointId;
 + (void)releasePointId:(NSInteger)pointId;
 @end
